56a1af1e49483b5a53990f18ecd0981388da55f4
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-06-27 13:39:03 +0200

    Update lemonde.lua

--- lemonde.lua
+++ lemonde.lua
@@ -1,29 +1,19 @@
-local xmlparser = require("xmlparser")
 local http = require("http")
+local xmlparser = require("xmlparser")
+local modem = peripheral.find("modem")
 
 local url = "https://www.lemonde.fr/rss/une.xml"
 
--- Fonction pour télécharger le fichier XML
-local function downloadXML(url)
-  local response = http.get(url)
-  if response then
-    local content = response.readAll()
-    response.close()
-    return content
-  end
-  return nil
-end
-
--- Fonction pour afficher le contenu sur le moniteur
-local function afficherContenu(contenu)
+-- Fonction pour afficher le contenu XML sur le moniteur
+local function afficherContenuXML(contenuXML)
   local moniteur = peripheral.wrap("top") -- Assurez-vous que le moniteur est connecté sur le dessus de l'ordinateur
   moniteur.setTextScale(0.5)
   moniteur.clear()
 
   local x, y = 1, 1
-  for i = 1, #contenu do
+  for i = 1, #contenuXML do
     moniteur.setCursorPos(x, y)
-    moniteur.write(contenu[i])
+    moniteur.write(contenuXML[i])
     y = y + 1
     if y > moniteur.getHeight() then
       break
@@ -33,13 +23,17 @@ end
 
 -- Fonction principale
 local function main()
-  local xmlContent = downloadXML(url)
-  if xmlContent then
-    local document = xmlparser.parse(xmlContent)
+  local success, response = http.get(url)
 
-    -- Obtention des titres des articles
-    local articles = document.children[1].children
+  if success then
+    local content = response.readAll()
+    response.close()
+
+    local document = xmlparser.parse(content)
+
+    -- Extraire le contenu XML souhaité (par exemple, les titres des articles)
     local titres = {}
+    local articles = document.children[1].children
     for i = 1, #articles do
       if articles[i].tag == "item" then
         for j = 1, #articles[i].children do
@@ -51,9 +45,10 @@ local function main()
       end
     end
 
-    afficherContenu(titres)
+    -- Afficher les titres des articles sur le moniteur
+    afficherContenuXML(titres)
   else
-    print("Erreur lors du téléchargement du fichier XML.")
+    print("Erreur lors de la requête HTTP.")
   end
 end
 
