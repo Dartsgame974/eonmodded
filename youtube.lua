8bb285b4a4004941e3b4bf1923e00d156b4c35f5
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-06-29 02:30:28 +0200

    Create youtube.lua

---
+++ youtube.lua
@@ -0,0 +1,41 @@
+-- Fonction pour récupérer les données d'une chaîne
+function getChannelData(channelIdentifier)
+    local url = "https://pipedapi.kavin.rocks/channel/" .. channelIdentifier
+    local response = http.get(url)
+    local data = response.readAll()
+    response.close()
+
+    local json = textutils.unserializeJSON(data)
+    if json then
+        return json
+    else
+        return nil
+    end
+end
+
+-- Fonction pour obtenir le nombre d'abonnés d'une chaîne
+function getSubscriberCount(channelData)
+    if channelData and channelData.subscriberCount then
+        return channelData.subscriberCount
+    else
+        return nil
+    end
+end
+
+-- Demander à l'utilisateur de saisir le nom ou l'ID de la chaîne
+print("Veuillez saisir le nom ou l'ID de la chaîne :")
+local channelIdentifier = read()
+
+-- Obtenir les données de la chaîne
+local channelData = getChannelData(channelIdentifier)
+if channelData then
+    -- Obtenir le nombre d'abonnés
+    local subscriberCount = getSubscriberCount(channelData)
+    if subscriberCount then
+        print("Nombre d'abonnés de la chaîne " .. channelIdentifier .. " : " .. subscriberCount)
+    else
+        print("Impossible d'obtenir le nombre d'abonnés de la chaîne.")
+    end
+else
+    print("Impossible de récupérer les données de la chaîne.")
+end
